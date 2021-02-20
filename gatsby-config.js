c08e70047ee270bd1fe2da8f845f15264219525b
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2021-02-20 15:09:35 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: "fitnfocused",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress-experimental",
+      options: {
+        url: " http://fitnfocused.local/graphql",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
